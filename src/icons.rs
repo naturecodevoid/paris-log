3988006a170ce0eb5fb41a5935696340b24ba7f1
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-04-06 08:00:11 -0700

    attempt to hide from editor autocomplete

--- src/icons.rs
+++ src/icons.rs
@@ -1,41 +1,41 @@
 #[macro_export]
 macro_rules! error {
     ($($arg:tt)*) => {
-        $crate::__private_exports::error!("{}", $crate::__private_exports::colorize_string(format!("<red><cross></> {}", format!($($arg)*))))
+        $crate::__private_exports_do_not_use::__export_error!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!("<red><cross></> {}", format!($($arg)*))))
     }
 }
 
 #[macro_export]
 macro_rules! warn {
     ($($arg:tt)*) => {
-        $crate::__private_exports::warn!("{}", $crate::__private_exports::colorize_string(format!("<yellow><warn></> {}", format!($($arg)*))))
+        $crate::__private_exports_do_not_use::__export_warn!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!("<yellow><warn></> {}", format!($($arg)*))))
     }
 }
 
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {
-        $crate::__private_exports::info!("{}", $crate::__private_exports::colorize_string(format!("<cyan><info></> {}", format!($($arg)*))))
+        $crate::__private_exports_do_not_use::__export_info!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!("<cyan><info></> {}", format!($($arg)*))))
     }
 }
 
 #[macro_export]
 macro_rules! success {
     ($($arg:tt)*) => {
-        $crate::__private_exports::info!("{}", $crate::__private_exports::colorize_string(format!("<green><tick></> {}", format!($($arg)*))))
+        $crate::__private_exports_do_not_use::__export_info!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!("<green><tick></> {}", format!($($arg)*))))
     }
 }
 
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)*) => {
-        $crate::__private_exports::debug!("{}", $crate::__private_exports::colorize_string(format!($($arg)*)))
+        $crate::__private_exports_do_not_use::__export_debug!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!($($arg)*)))
     }
 }
 
 #[macro_export]
 macro_rules! trace {
     ($($arg:tt)*) => {
-        $crate::__private_exports::trace!("{}", $crate::__private_exports::colorize_string(format!($($arg)*)))
+        $crate::__private_exports_do_not_use::__export_trace!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!($($arg)*)))
     }
 }
--- src/normal.rs
+++ src/normal.rs
@@ -1,34 +1,34 @@
 #[macro_export]
 macro_rules! error {
     ($($arg:tt)*) => {
-        $crate::__private_exports::error!("{}", $crate::__private_exports::colorize_string(format!($($arg)*)))
+        $crate::__private_exports_do_not_use::__export_error!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!($($arg)*)))
     }
 }
 
 #[macro_export]
 macro_rules! warn {
     ($($arg:tt)*) => {
-        $crate::__private_exports::warn!("{}", $crate::__private_exports::colorize_string(format!($($arg)*)))
+        $crate::__private_exports_do_not_use::__export_warn!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!($($arg)*)))
     }
 }
 
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {
-        $crate::__private_exports::info!("{}", $crate::__private_exports::colorize_string(format!($($arg)*)))
+        $crate::__private_exports_do_not_use::__export_info!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!($($arg)*)))
     }
 }
 
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)*) => {
-        $crate::__private_exports::debug!("{}", $crate::__private_exports::colorize_string(format!($($arg)*)))
+        $crate::__private_exports_do_not_use::__export_debug!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!($($arg)*)))
     }
 }
 
 #[macro_export]
 macro_rules! trace {
     ($($arg:tt)*) => {
-        $crate::__private_exports::trace!("{}", $crate::__private_exports::colorize_string(format!($($arg)*)))
+        $crate::__private_exports_do_not_use::__export_trace!("{}", $crate::__private_exports_do_not_use::__export_colorize_string(format!($($arg)*)))
     }
 }
